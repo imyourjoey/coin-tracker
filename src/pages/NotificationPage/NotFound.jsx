1c81d425b390929ce6e81ef1f28550d443475acb
imyourjoey <joeylim016@gmail.com>
2024-10-08 15:03:19 +0800

    add not found oage

--- src/pages/NotificationPage/NotFound.jsx
+++ src/pages/NotificationPage/NotFound.jsx
@@ -1,7 +1,17 @@
+import IconNotFound from "../../assets/Icons/IconNotFound";
+
 function NotFound() {
   return (
     <>
-      <div>this is not found page</div>
+      <div className="flex justify-center">
+        <div>
+          <IconNotFound width="400" height="350" />
+          <div className="text-center text-3xl font-semibold">Whoops!</div>
+          <div className="text-center mt-2 text-gray-500">
+            The page you're looking for does not exist!
+          </div>
+        </div>
+      </div>
     </>
   );
 }
